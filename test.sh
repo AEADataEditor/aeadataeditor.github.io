1089f003a3f5ad4d812a25b901efa49194d39f9f
Lars Vilhuber <lars@vilhuber.com>
2021-07-16 14:44:14 -0400

    Messing with the test Docker

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 key=$HOME/Dropbox/.myconfig/github-docker-secret
-docker build -t aeaweb .
+DOCKER_BUILDKIT=1 docker build -t aeaweb . -f Dockerfile.step2
 docker run -e JEKYLL_GITHUB_TOKEN=$(cat $key) --volume="$PWD:/usr/src/app" -p 4000:4000 -it aeaweb
